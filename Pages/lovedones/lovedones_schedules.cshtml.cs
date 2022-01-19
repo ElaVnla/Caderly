3d048b587d8dc19079f43c0df43095ac57412edf
erikasampang <66808599+erikasampang@users.noreply.github.com>
2022-01-19 10:53:59 +0800

    createbooking and database

--- Pages/lovedones/lovedones_schedules.cshtml.cs
+++ Pages/lovedones/lovedones_schedules.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Caderly.Pages
+namespace Caderly.Pages.lovedones
 {
     public class lovedones_schedulesModel : PageModel
     {
